f82a1a2ddc114d02cb8d9919c20047b2a8a1deee
Programming Hero <programming.hero1@gmail.com>
2022-04-03 00:29:24 -0500

    simple search

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,13 +5,23 @@ import './Main.css';
 
 const Main = () => {
     const [volunteers] = useVolunteers();
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = event =>{
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.includes(searchText));
+        setSearchResult(match);
+    }
 
     return (
         <div>
             <h2>Volunteer Activities: {volunteers.length}</h2>
+            <div style={{'margin': '20px'}}>
+                <input onChange={handleSearchChange} type="text" placeholder='search'/>
+            </div>
             <div className="activity-container">
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity._id}
                         activity={activity}
                     ></Activity>)
