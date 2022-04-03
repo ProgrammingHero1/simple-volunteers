39db6e7e07419c8b56329d9a40c2051c45457121
Programming Hero <programming.hero1@gmail.com>
2022-04-03 08:20:42 -0500

    last commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,18 +4,33 @@ import Activity from '../Activity/Activity';
 import './Main.css';
 
 const Main = () => {
-    const [volunteers] = useVolunteers();
+    // const [volunteers] = useVolunteers();
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect( () =>{
+        console.log('inside use Effect')
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => {
+            const match = data.filter(d => d.title.includes(searchText));
+            setSearchResult(match);
+        })
+    }, [searchText])
+
     const handleSearchChange = event =>{
-        const searchText = event.target.value;
-        const match = volunteers.filter(v => v.title.includes(searchText));
-        setSearchResult(match);
+        setSearchText(event.target.value);
     }
+    
+    // const handleSearchChange2 = event =>{
+    //     const searchText = event.target.value;
+    //     const match = volunteers.filter(v => v.title.includes(searchText));
+    //     setSearchResult(match);
+    // }
 
     return (
         <div>
-            <h2>Volunteer Activities: {volunteers.length}</h2>
+            <h2>Volunteer Activities: {searchResult.length}</h2>
             <div style={{'margin': '20px'}}>
                 <input onChange={handleSearchChange} type="text" placeholder='search'/>
             </div>
